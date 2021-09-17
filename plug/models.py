de8fdcf034858dda7d34050a0386996e0c3ff17c
wilson <whippet@plydot.com>
2021-09-17 14:29:21 +0300

    align message response

--- plug/models.py
+++ plug/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+
+def get_outbound_message(message_id, time, uuid, recipient, dispatch_date, sent_status, error_message):
+    return {
+        "messageId": message_id,
+        "type": "OUTBOUND",
+        "date": time,
+        "text": "req.message",
+        "uuid": uuid,
+        "encoding": "ENC7BIT",
+        "dstPort": -1,
+        "srcPort": -1,
+        "recipient": recipient,
+        "dispatchDate": dispatch_date,
+        "validityPeriod": -1,
+        "statusReport": sent_status,
+        "from": "",
+        "messageStatus": "SUCCESS" if sent_status else "FAILED",
+        "failureCause": "" if sent_status else "NO_ROUTE",
+        "retryCount": 0,
+        "priority": 0,
+        "refNo": "",
+        "errorMessage": error_message,
+        "scheduledDeliveryDate": dispatch_date,
+        "flashSms": False,
+        "pduUserDataHeader": None,
+        "pduUserData": None,
+        "deliveryDelay": 0,
+        "dcsmessageClass": "MSGCLASS_NONE",
+        "gatewayId": "*"
+    }
--- plug/views.py
+++ plug/views.py
@@ -1,51 +1,71 @@
 from __future__ import print_function
 
 import json
+import uuid
+
+from django.utils import timezone
 
 import gammu
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from plug.models import get_outbound_message
 from sms import settings
 
 
 @csrf_exempt
 def send(request=None):
-    data = json.loads(request.body)[0]
-    network = "others"
-    recipient = str(data.get("recipient"))
-    if recipient.startswith("256"):
-        if recipient.startswith("25678") or recipient.startswith("25677"):
-            network = "mtn"
-        elif recipient.startswith("25675") or recipient.startswith("25670"):
-            network = "airtel"
+    _data = json.loads(request.body)
+    to_send = []
+    for data in _data:
+        network = "others"
+        recipient = str(data.get("recipient"))
+        if recipient.startswith("256"):
+            if recipient.startswith("25678") or recipient.startswith("25677"):
+                network = "mtn"
+            elif recipient.startswith("25675") or recipient.startswith("25670"):
+                network = "airtel"
 
-    message = data.get("message")
-    message_id = data.get("id")
-    state_machine = gammu.StateMachine()
-    config = f"{settings.BASE_DIR / 'gammu/.gammurc'}_{network}"
-    state_machine.ReadConfig(Filename=config)
-    state_machine.Init()
-    message = {
-        "Text": message,
-        "SMSC": {"Location": 1},
-        "Number": int(recipient),
-    }
+        __message = data.get("message")
+        message_id = data.get("id")
+        state_machine = gammu.StateMachine()
+        config = f"{settings.BASE_DIR / 'gammu/.gammurc'}_{network}"
+        state_machine.ReadConfig(Filename=config)
+        state_machine.Init()
+        _message = {
+            "Text": __message,
+            "SMSC": {"Location": 1},
+            "Number": int(recipient),
+            "id": message_id
+        }
+        to_send.append(_message)
     # Actually send the message
-    try:
-        reference = state_machine.SendSMS(message)
-        return JsonResponse(
-            {
-                "sent": True if reference else None,
-                "reference": reference,
-                "id": message_id
-            }
-        )
-    except Exception as e:
-        return JsonResponse(
-            {
-                "sent": False,
-                "message": json.loads(json.dumps(str(e))),
-                "id": message_id
-            }
-        )
+    sent = 0
+    failed = 0
+    total = len(to_send)
+    outbound_messages = []
+    for message in to_send:
+        try:
+            reference = state_machine.SendSMS(message)
+            sent = sent+1 if reference else sent+0
+            sent_status = True if reference else False
+            outbound_messages.append(
+                get_outbound_message(reference, timezone.now(), message.get("id"),
+                                     message.get("Number"), timezone.now(), sent_status, "")
+            )
+        except Exception as e:
+            failed = failed + 1
+            sent_status = False
+            outbound_messages.append(
+                get_outbound_message(None, timezone.now(), message.get("id"),
+                                     message.get("Number"), timezone.now(), sent_status,
+                                     json.loads(json.dumps(str(e))))
+            )
+    return JsonResponse({
+        "sent": sent,
+        "id": uuid.UUID(),
+        "provider": None,
+        "sender": "",
+        "total": total,
+        "failed": failed
+    })
